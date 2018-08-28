f69a84cd640a226931cbfdb452f16f720bfe7de1
navegador <navegador@030_01_27_72729>
2018-08-27 22:16:45 -0300

    Add Mvc Proyect Web ite

---
+++ Presentation/Mcga.Website/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Mcga.Website.Startup))]
+namespace Mcga.Website
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
